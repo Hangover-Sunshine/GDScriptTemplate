717073766487870a98c6f70aeaf9e8ef6ccb5a53
MichaelGardone <michaelgardone@gmail.com>
2024-08-15 23:25:20 -0600

    replaced SceneTransitionSystem with Verho

--- scripts/autoloads/GlobalSignals.gd
+++ scripts/autoloads/GlobalSignals.gd
@@ -1,9 +1,1 @@
 extends Node
-
-# System Loading ###################################################
-signal load_scene(next_scene:String)
-signal load_scene_with_anim(next_scene:String, anim_to_use:String)
-signal scene_loaded(new_scene:String)
-signal scene_transition_started
-signal scene_transition_done
-# System Loading ###################################################
